66a6315ec487c117585c45be18681599f1033b20
mynspluto <mynspluto@gmail.com>
2025-03-26 23:17:42 +0900

    게시글이 10 개 미만 버그픽스스

--- index.js
+++ index.js
@@ -33,8 +33,10 @@ const parser = new Parser({
 
   text += `<ul>`;
 
+  const postCount = Math.min(feed.items.length, 10);
+
   // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < postCount; i++) {
     const { title, link } = feed.items[i];
     console.log(`${i + 1}번째 게시물`);
     console.log(`추가될 제목: ${title}`);
